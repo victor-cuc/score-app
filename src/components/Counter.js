8681ac40f2f39a7d340d67010aa273502f70d792
Victor Cuc <victorcuc99@yahoo.com>
2018-06-14 01:00:33 +0300

    Initial commit

---
+++ src/components/Counter.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Counter = (props) => {
+  return (
+    <div className="counter" >
+      <button 
+        className="counter-action decrement" 
+        onClick={ () => props.updatePlayerScore(props.index, -1) }>
+        -
+      </button>
+      <div className="counter-score"> {props.score} </div>
+      <button 
+        className="counter-action increment" 
+        onClick={ () => props.updatePlayerScore(props.index, 1) }>
+        +
+      </button>
+    </div>
+  );
+ }
+ 
+ Counter.propTypes = {
+   updatePlayerScore: PropTypes.func.isRequired,
+   index: PropTypes.number.isRequired,
+   score: PropTypes.number.isRequired,
+ };
+
+ export default Counter;
---
+++ src/containers/Scoreboard.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { bindActionCreators } from 'redux';
+import * as PlayerActionCreators from '../actions/player';
+import { connect } from 'react-redux';
+import AddPlayerForm from '../components/AddPlayerForm';
+import Player from '../components/Player';
+import Header from '../components/Header'
+
+
+class Scoreboard extends Component {
+
+  static propTypes = {
+    players: PropTypes.array.isRequired,
+  };
+
+  render() {
+    const {dispatch, players} = this.props;
+    const addPlayer = bindActionCreators(PlayerActionCreators.addPlayer, dispatch);
+    const removePlayer = bindActionCreators(PlayerActionCreators.removePlayer, dispatch);
+    const updatePlayerScore = bindActionCreators(PlayerActionCreators.updatePlayerScore, dispatch);
+
+    const playerComponents = players.map((player, index) => (
+      <Player 
+        index={index} 
+        name={player.name} 
+        score={player.score} 
+        key={player.name}
+        updatePlayerScore={updatePlayerScore}
+        removePlayer={removePlayer} 
+      />
+    ));
+
+    return (
+      <div className="scoreboard">
+        <Header players={players} />
+        <div className="players">
+          { playerComponents }
+        </div>
+        <AddPlayerForm addPlayer={addPlayer} />
+      </div>
+    );
+  }
+};
+
+
+const mapStateToProps = (state) => (
+  {
+    players: state
+  }
+);
+
+export default connect(mapStateToProps)(Scoreboard);
